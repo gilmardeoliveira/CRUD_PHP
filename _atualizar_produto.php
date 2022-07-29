452e56986e5eb89d20f4d39e02939589886e56e2
Gilmar Moraes de Oliveira Júnior <gilmar.ol.jr@gmail.com>
2022-07-29 00:19:18 -0300

    adicionado página atualizar produto

--- _atualizar_produto.php
+++ _atualizar_produto.php
@@ -1,3 +1,11 @@
 <?php
 
+    $id = $_POST['id'];
+    //$nroproduto = $_POST['nroproduto'];
+    $nomeproduto = $_POST['nomeproduto'];
+    $categoria = $_POST['categoria'];
+    $quantidade = $_POST['quantidade'];
+    $fornecedor = $_POST['fornecedor']
+
+    $sql = "UPDATE `estoque` SET `nomeproduto`='$nomeproduto',`categoria`='$categoria',`quantidade`=$quantidade,`fornecedor`='$fornecedor' WHERE id_estoque = $id";
 ?>
