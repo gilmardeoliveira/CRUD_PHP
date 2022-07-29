e253953cbd73383b543be3d99780b3d0bede5c4e
Gilmar Moraes de Oliveira Júnior <gilmar.ol.jr@gmail.com>
2022-07-29 00:20:08 -0300

    alterado fornecedor

--- _inserir_produto.php
+++ _inserir_produto.php
@@ -2,11 +2,11 @@
 
 include 'conexao.php';
 
-$nroproduto = $_POST['nroproduto']; //Recebe o valor do atributo
+$nroproduto = $_POST['nroproduto'];
 $nomeproduto = $_POST['nomeproduto'];
 $categoria = $_POST['categoria'];
 $quantidade = $_POST['quantidade'];
-$fornecedor = $_POST['forncedor'];
+$fornecedor = $_POST['fornecedor'];
 
 $sql = "INSERT INTO `estoque`( `nroproduto`, `nomeproduto`, `categoria`, `quantidade`, `fornecedor`) VALUES ($nroproduto,'$nomeproduto','$categoria',$quantidade,'$fornecedor')";
 $inserir = mysqli_query($conexao,$sql);
