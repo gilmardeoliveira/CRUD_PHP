2ab8e6e2dc1e648209563ec9242fb70ca2d25ada
Gilmar Moraes de Oliveira Júnior <gilmar.ol.jr@gmail.com>
2022-08-01 22:17:08 -0300

    adicionado página "_atualizar_produto.php"

--- _atualizar_produto.php
+++ _atualizar_produto.php
@@ -1,11 +1,26 @@
 <?php
 
+    include 'conxao.php';
+
     $id = $_POST['id'];
     //$nroproduto = $_POST['nroproduto'];
     $nomeproduto = $_POST['nomeproduto'];
     $categoria = $_POST['categoria'];
     $quantidade = $_POST['quantidade'];
-    $fornecedor = $_POST['fornecedor']
+    $fornecedor = $_POST['fornecedor'];
 
-    $sql = "UPDATE `estoque` SET `nomeproduto`='$nomeproduto',`categoria`='$categoria',`quantidade`=$quantidade,`fornecedor`='$fornecedor' WHERE id_estoque = $id";
+    $sql="UPDATE `estoque` SET `nomeproduto`='$nomeproduto',`categoria`='$categoria',`quantidade`=$quantidade,`fornecedor`='$fornecedor' WHERE id_estoque = $id";
+
+    $atualizar = mysqli_query($conexao,$sql);
 ?>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+<div class="container" style="widht: 400px;">
+    <center>
+        <h3>Atualizado com sucesso</h3>
+            <div style="margin-top: 10px">
+                <a href="lista_produtos.php" class="btn btn-sm btn-warning" style="color:#fff">Voltar</a>
+            </div>
+    </center>
+
+</div>
