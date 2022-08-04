a3e29a1e3fd78ad837a7468fddbf314aacd8219d
Gilmar Moraes de Oliveira Júnior <gilmar.ol.jr@gmail.com>
2022-08-04 20:02:06 -0300

    adicionado _adicionar_fornecedores.php

---
+++ adicionar_fornecedores.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+    <div class=container>
+         <div style="text-align: right;margin-top: 40px" >
+                <a href="index.php" role="button" class=" btn btn-sm btn-primary">Voltar</a>
+        </div><!--botão voltar-->
+        <form action="" method="post" style="margin-top: 40px">
+        <h4>Adicionar Fornecedores</h4>
+        <input type="text" name="fornecedor" class="form-control" placeholder="Insira o fornecedor">
+        
+        </form>
+    </div>
+    
+</body>
+</html>
