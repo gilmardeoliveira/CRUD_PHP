cc2bb58c0cab8f8c149732481857de4de092dcf1
Gilmar Moraes de Oliveira Júnior <gilmar.ol.jr@gmail.com>
2022-08-05 15:04:13 -0300

    corrigido erros dentro do código

--- adicionar_produto.php
+++ adicionar_produto.php
@@ -68,9 +68,17 @@
 				<div class="form-group">
 					<label>Fornecedor</label>
 					<select class="form-control" name="fornecedor" required autocomplete="off">
-					
-						<option>#</option>
-						
+						<?php
+						include 'conexao.php';
+						$sql2 = "SELECT * FROM fornecedor order by nome_fornecedor ASC";
+						$buscar2 = mysqli_query($conexao,$sql2);
+
+						while ($array2 = mysqli_fetch_array($buscar2)) {
+							$id_fornecedor = $array2['id_fornecedor'];
+							$nome_fornecedor = $array2['nome_fornecedor'];
+						?>		
+						<option><?php echo $nome_fornecedor ?></option>
+						<?php } ?>
 					</select><!--Select fornecedor-->
 				</div>
 							<br>
